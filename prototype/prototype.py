bc388725e9a7c6f157607013eeb4e315949f0193
Lee Newberg <lee.newberg@kitware.com>
2024-12-02 15:18:22 -0500

    ENH: WIP: Prototype workflow

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -18,6 +18,7 @@
 # Import from Python packages
 from dipy.io.image import load_nifti, save_nifti
 import csv
+import math
 import numpy as np
 import random
 import re
@@ -29,11 +30,28 @@ coregistered_images_directory = os.path.join(gor_image_directory, "coregistered-
 tabular_data_directory = "/data2/ABCD/abcd-5.0-tabular-data-extracted"
 core_directory = os.path.join(tabular_data_directory, "core")
 
+# The locations of some useful csv data files
+file_mh_y_ksads_ss = os.path.join(
+    core_directory, "mental-health/mh_y_ksads_ss.csv"
+)  # see `interesting_ksads` below
+file_abcd_y_lt = os.path.join(
+    core_directory, "abcd-general/abcd_y_lt.csv"
+)  # site_id_l, rel_family_id, interview_age
+file_abcd_p_demo = os.path.join(
+    core_directory, "abcd-general/abcd_p_demo.csv"
+)  # demo_gender_id_v2(_l)?
+file_gish_p_gi = os.path.join(
+    core_directory, "gender-identity-sexual-health/gish_p_gi.csv"
+)  # demo_gender_id_v2(_l)?
+ph_y_bld = os.path.join(core_directory, "physical-health/ph_y_bld.csv")
 
 # +
 # Set global parameters to guide the workflow
 
 # +
+# Functions for handling image voxel data
+
+
 def get_list_of_image_files(directory):
     response = [
         os.path.join(directory, file)
@@ -41,6 +59,25 @@ def get_list_of_image_files(directory):
         if file.endswith(".nii.gz")
     ]
     return response
+
+
+def fix_filename_parsing(filename_parsing_dict):
+    if "src_subject_id" in filename_parsing_dict.keys():
+        filename_parsing_dict["src_subject_id"] = filename_parsing_dict[
+            "src_subject_id"
+        ].replace("NDAR", "NDAR_")
+    eventname_conversion = {
+        "baselineYear1Arm1": "baseline_year_1_arm_1",
+        "1YearFollowUpYArm1": "1_year_follow_up_y_arm_1",
+        "2YearFollowUpYArm1": "2_year_follow_up_y_arm_1",
+        "3YearFollowUpYArm1": "3_year_follow_up_y_arm_1",
+        "4YearFollowUpYArm1": "4_year_follow_up_y_arm_1",
+    }
+    if "eventname" in filename_parsing_dict.keys():
+        filename_parsing_dict["eventname"] = eventname_conversion[
+            filename_parsing_dict["eventname"]
+        ]
+    return filename_parsing_dict
 
 
 def get_data_from_image_files(list_of_files):
@@ -56,8 +93,8 @@ def get_data_from_image_files(list_of_files):
     filename_keys = (
         "gorinput",
         "modality",
-        "subject",
-        "session",
+        "src_subject_id",
+        "eventname",
         "run",
         "image_type",
         "image_subtype",
@@ -66,10 +103,14 @@ def get_data_from_image_files(list_of_files):
     response = [
         (file,)
         + (
-            dict(
-                zip(
-                    filename_keys,
-                    list(re.match(filename_pattern, os.path.basename(file)).groups()),
+            fix_filename_parsing(
+                dict(
+                    zip(
+                        filename_keys,
+                        list(
+                            re.match(filename_pattern, os.path.basename(file)).groups()
+                        ),
+                    )
                 )
             ),
         )
@@ -79,9 +120,28 @@ def get_data_from_image_files(list_of_files):
     return response
 
 
-def csv_file_to_raw_data(filename):
+def select_images(image_data, key_dict):
+    # Currently we support only two keys
+    assert all(
+        [
+            key == "src_subject_id" or key == "eventname"
+            for key, value in key_dict.items()
+        ]
+    )
+    response = [
+        image_row
+        for image_row in image_data
+        if all(item in image_row.items() for item in key_dict.items())
+    ]
+    return response
+
+# +
+# Functions for reading and selecting data from csv files
+
+
+def _csv_file_to_list_of_lists(filename):
     """
-    csv_file_to_raw_data returns a list of lists of str in row-major order
+    _csv_file_to_list_of_lists returns a list of lists of str in row-major order
     """
     with open(filename, mode="r") as file:
         csv_reader = csv.reader(file)
@@ -89,62 +149,152 @@ def csv_file_to_raw_data(filename):
     return response
 
 
-def csv_raw_to_row_dicts(csv_raw):
+def _list_of_lists_to_row_dicts(list_of_lists):
     """
-    csv_raw_to_row_dicts converts a list of lists of str, in row-major order, to a list of dict of str.
-    Each dict is formed by using the first row (column headers) as keys
-    and a given data row as values of type str.
+    _list_of_lists_to_row_dicts converts a list of lists of str, in row-major order,
+    to a list of dict of str.  Each dict is formed by using the first row (column headers)
+    as keys and a given data row as values of type str.
     """
-    column_headers = csv_raw[0]
+    column_headers = list_of_lists[0]
     response = [
         {column_headers[i]: row_value for i, row_value in enumerate(row)}
-        for row in csv_raw[1:]
+        for row in list_of_lists[1:]
     ]
     return response
 
 
 def csv_file_to_row_dicts(file):
-    return csv_raw_to_row_dict(csv_file_to_raw_data(file))
+    return _list_of_lists_to_row_dicts(_csv_file_to_list_of_lists(file))
 
 
-def get_items(csv_row_dicts, key_dict):
+def select_items(list_of_row_dicts, key_dict):
     """
-    get_items returns all rows (i.e., top-level elements) of csv_row_dicts, a list of dict of str,
+    select_items returns all rows (i.e., top-level elements) of list_of_row_dicts, a list of dict of str,
     for which the row (a dict) contains all of the items in key_dict.
     """
     response = [
         row_dict
-        for row_dict in csv_row_dicts
+        for row_dict in list_of_row_dicts
         if all(item in row_dict.items() for item in key_dict.items())
     ]
     return response
 
+# +
+# Functions for computing summary statistics for csv data
+
+
+def row_dicts_to_counts_for_each_column(row_dicts):
+    column_dict = {key: dict() for row in row_dicts for key in row.keys()}
+    for row in row_dicts:
+        for column_header, value in row.items():
+            # column_dict[column_header] = column_dict.get(column_header, dict())
+            if value == "555" or value == "888":
+                value = ""
+            column_dict[column_header][value] = (
+                column_dict[column_header].get(value, 0) + 1
+            )
+    # Sort each value dict by its keys for easier reading
+    column_dict = {
+        key: dict(sorted(value.items(), key=lambda item: item[0], reverse=False))
+        for key, value in column_dict.items()
+    }
+    return column_dict
+
+
+def entropy_of_column_counts(column_counts):
+    assert all(value >= 0 for value in column_counts.values())
+    total_count = sum(column_counts.values())
+    entropy = sum(
+        [
+            count / total_count * math.log2(total_count / count)
+            for count in column_counts.values()
+            if count > 0
+        ]
+    )
+    return entropy
+
+
+def entropy_of_all_columns(all_columns):
+    return {
+        key: entropy_of_column_counts(value)
+        for key, value in all_columns.items()
+        if bool(re.match("ksads_\d", key))
+    }
+
+
+def find_interesting_entropies(file_mh_y_ksads_ss):
+    # Find some KSADS data columns with high entropy.
+    # TODO: Should we be treating the value '888' or '' specially?
+    row_dicts_mh_y_ksads_ss = csv_file_to_row_dicts(file_mh_y_ksads_ss)
+    print("Read done")
+
+    counts_for_each_column_mh_y_ksads_ss = row_dicts_to_counts_for_each_column(
+        row_dicts_mh_y_ksads_ss
+    )
+    print("Column counting done")
+
+    entropies = entropy_of_all_columns(counts_for_each_column_mh_y_ksads_ss)
+    sorted_entropies = dict(
+        sorted(entropies.items(), key=lambda item: item[1], reverse=True)
+    )
+    sorted_entropies = {
+        key: (value, counts_for_each_column_mh_y_ksads_ss[key])
+        for key, value in sorted_entropies.items()
+        if bool(re.match("ksads_\d", key))
+    }
+    print("Entropy calculation done")
+    return sorted_entropies
+
 
 # +
+# Find interesting KSADS data
+
+if True:
+    # We want to re-compute interesting_ksads
+    try:
+        sorted_entropies
+        print("Using cached value for sorted_entropies")
+    except NameError:
+        # find_interesting_entropies is slow, so only compute sorted_entropies if we haven't already
+        sorted_entropies = find_interesting_entropies(file_mh_y_ksads_ss)
+    number_wanted = 20
+    # print(f"Distribution details: {list(sorted_entropies.items())[:number_wanted]}")
+    interesting_ksads = list(sorted_entropies.keys())[:number_wanted]
+    print(f"{interesting_ksads = }")
+else:
+    # Previous expression returns
+    interesting_ksads = [
+        "ksads_22_142_t",  # Symptom - Insomnia, Past
+        "ksads_22_970_t",  # Diagnosis - SLEEP PROBLEMS, Past
+        "ksads_2_11_t",  # Symptom - Explosive Irritability, Past
+        "ksads_1_2_t",  # Symptom - Depressed Mood, Past
+        "ksads_2_13_t",  # Symptom - Decreased Need for Sleep, Past
+        "ksads_1_6_t",  # Symptom - Anhedonia, Past
+        "ksads_22_141_t",  # Symptom - Insomnia, Present
+        "ksads_22_969_t",  # Diagnosis - SLEEP PROBLEMS, Present
+        "ksads_2_8_t",  # Symptom - Elevated Mood, Past
+        "ksads_10_46_t",  # Symptom - Excessive worries more days than not Past
+        "ksads_1_4_t",  # Symptom - Irritability, Past
+        "ksads_2_10_t",  # Symptom - ExplosiveIrritability, PresentNext
+        "ksads_8_31_t",  # Symptom - Fear of Social Situations, Past
+        "ksads_23_146_t",  # Symptom - Wishes/Better off dead, Past
+        "ksads_23_957_t",  # Diagnosis - SuicidalideationPassivePast
+        "ksads_1_5_t",  # Symptom - Anhedonia, Present
+        "ksads_2_839_t",  # Diagnosis - Unspecified Bipolar and Related Disorder, PAST (F31.9)
+        "ksads_2_833_t",  # Diagnosis - Bipolar I Disorder, most recent past episode manic (F31.1x)
+        "ksads_1_3_t",  # Symptom - Irritability, Present
+        "ksads_1_842_t",  # Diagnosis - Major Depressive Disorder, Past (F32.9)
+    ]
+# -
+
+# A random subset of all image files
 list_of_image_files = random.sample(
     get_list_of_image_files(coregistered_images_directory), 10
 )
 image_data = get_data_from_image_files(list_of_image_files)
+print(f"file name parsing = {[image[1] for image in image_data]!r}")
 
-file_mh_y_ksads_ss = os.path.join(
-    core_directory, "mental-health/mh_y_ksads_ss.csv"
-)  # ksads_1_5_t (anhedonia), something with good entropy!!!
-file_abcd_y_lt = os.path.join(
-    core_directory, "abcd-general/abcd_y_lt.csv"
-)  # site_id_l, rel_family_id, interview_age
-file_abcd_p_demo = os.path.join(
-    core_directory, "abcd-general/abcd_p_demo.csv"
-)  # demo_gender_id_v2(_l)?
-file_gish_p_gi = os.path.join(
-    core_directory, "gender-identity-sexual-health/gish_p_gi.csv"
-)  # demo_gender_id_v2(_l)?
-
-csv_file = os.path.join(core_directory, "physical-health/ph_y_bld.csv")
-csv_raw = csv_file_to_raw_data(csv_file)
-row_dicts = csv_raw_to_row_dicts(csv_raw)
-# -
-
-if True:
+if False:
     print(f"{len(image_data) = }")
     print(f"{type(image_data[0]) = }")
     print(f"{len(image_data[0]) = }")
@@ -153,13 +303,13 @@ if True:
     print(f"{image_data[0][1] = }")
     print(f"{image_data[0][2].shape = }")
     print(f"{image_data[0][3].shape = }")
-if True:
-    print(f"{type(csv_raw) = }")
-    print(f"{type(csv_raw[0]) = }")
-    # print("csv_data row lengths = ", [len(row) for row in csv_raw])
-    # print("Column 0 = ", [row[0] for row in csv_raw])
-    print("Corner value = ", csv_raw[0][0])
-if True:
+if False:
+    print(f"{type(list_of_lists) = }")
+    print(f"{type(list_of_lists[0]) = }")
+    # print("csv_data row lengths = ", [len(row) for row in list_of_lists])
+    # print("Column 0 = ", [row[0] for row in list_of_lists])
+    print("Corner value = ", list_of_lists[0][0])
+if False:
     print(f"{type(row_dicts) = }")
     key0 = 0
     value0 = row_dicts[key0]
@@ -169,7 +319,7 @@ if True:
     # print(f"{row_dicts[0] = }")
     dict_of_keys = {key1: value1}
     print(f"{dict_of_keys = }")
-    items = get_items(row_dicts, dict_of_keys)
+    items = select_items(row_dicts, dict_of_keys)
     print(f"{len(items) = }")
 
 
