e541bf0d0b5713473970a1c2732a3103a7c02175
Lee Newberg <lee.newberg@kitware.com>
2024-12-03 09:51:31 -0500

    ENH: WIP: Prototype workflow

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -15,9 +15,21 @@
 
 # # An example workflow for generating hypotheses with ABCD data
 
+# ## Installing dependencies
+# One way to do this is with a virtual python environment installed so that it is accessible to Jupyter lab.  This needs to be set up only once.
+# ```bash
+# python -m venv ~/abcd311
+# source ~/abcd311/bin/activate
+# pip install ipykernel
+# python -m ipykernel install --user --name abcd311
+# pip install -r requirements.txt
+# ```
+# Then, once Jupyter is open with this lab notebook, "Change Kernel..." to be "abcd311".
+
 # Import from Python packages
 from dipy.io.image import load_nifti, save_nifti
 import csv
+import functools
 import math
 import numpy as np
 import random
@@ -30,23 +42,54 @@ coregistered_images_directory = os.path.join(gor_image_directory, "coregistered-
 tabular_data_directory = "/data2/ABCD/abcd-5.0-tabular-data-extracted"
 core_directory = os.path.join(tabular_data_directory, "core")
 
-# The locations of some useful csv data files
-file_mh_y_ksads_ss = os.path.join(
-    core_directory, "mental-health/mh_y_ksads_ss.csv"
-)  # see `interesting_ksads` below
-file_abcd_y_lt = os.path.join(
-    core_directory, "abcd-general/abcd_y_lt.csv"
-)  # site_id_l, rel_family_id, interview_age
-file_abcd_p_demo = os.path.join(
-    core_directory, "abcd-general/abcd_p_demo.csv"
-)  # demo_gender_id_v2(_l)?
-file_gish_p_gi = os.path.join(
-    core_directory, "gender-identity-sexual-health/gish_p_gi.csv"
-)  # demo_gender_id_v2(_l)?
-ph_y_bld = os.path.join(core_directory, "physical-health/ph_y_bld.csv")
-
 # +
-# Set global parameters to guide the workflow
+# The locations of some useful csv data columns
+
+# These files live in `core_directory`
+
+independent_vars = [
+    [
+        "abcd-general/abcd_y_lt.csv",
+        [
+            "site_id_l",  # Site ID at each event
+            # TODO: We are including participants with no siblings in study at the expense of losing family ID.  Do this better.
+            # "rel_family_id",  # Participants belonging to the same family share a family ID.  They will differ between data releases
+            "interview_age",  # Participant's age in month at start of the event
+        ],
+    ],
+    [
+        "abcd-general/abcd_p_demo.csv",
+        [
+            "demo_gender_id_v2",  # 1=Male; 2=Female; 3=Trans male; 4=Trans female; 5=Gender queer; 6=Different; 777=Decline to answer; 999=Don't know
+            # "demo_gender_id_v2_l",  # same?
+        ],
+    ],
+    [
+        "gender-identity-sexual-health/gish_p_gi.csv",
+        [
+            "demo_gender_id_v2",  # 1=Male; 2=Female; 3=Trans male; 4=Trans female; 5=Gender queer; 6=Different; 777=Decline to answer; 999=Don't know
+            # "demo_gender_id_v2_l",  # same?
+        ],
+    ],
+    [
+        "physical-health/ph_y_bld.csv",
+        [
+            # "biospec_blood_baso_percent",  # BASO %
+            # "biospec_blood_baso_abs",  # BASO ABS
+            # "biospec_blood_eos_percent",  # EOS %
+            # "biospec_blood_eos_abs",  # EOS ABS
+            # "biospec_blood_hemoglobin",  # Hemoglobin
+            # "biospec_blood_mcv",  # MCV
+            # "biospec_blood_plt_count",  # PLT Count
+            # "biospec_blood_wbc_count",  # WBC Count
+            # "biospec_blood_ferritin",  # Ferritin
+            # "biospec_blood_hemoglobin_a1",  # hemoglobin_a1
+            # "biospec_blood_imm_gran_per",  # Immature Gran %
+        ],
+    ],
+]
+
+# We'll do KSADS variables once we've computed `interesting_ksads` below
 
 # +
 # Functions for handling image voxel data
@@ -54,18 +97,16 @@ ph_y_bld = os.path.join(core_directory, "physical-health/ph_y_bld.csv")
 
 def get_list_of_image_files(directory):
     response = [
-        os.path.join(directory, file)
-        for file in os.listdir(directory)
-        if file.endswith(".nii.gz")
+        os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".nii.gz")
     ]
     return response
 
 
 def fix_filename_parsing(filename_parsing_dict):
     if "src_subject_id" in filename_parsing_dict.keys():
-        filename_parsing_dict["src_subject_id"] = filename_parsing_dict[
-            "src_subject_id"
-        ].replace("NDAR", "NDAR_")
+        filename_parsing_dict["src_subject_id"] = re.sub(
+            r"^NDAR", "NDAR_", filename_parsing_dict["src_subject_id"]
+        )
     eventname_conversion = {
         "baselineYear1Arm1": "baseline_year_1_arm_1",
         "1YearFollowUpYArm1": "1_year_follow_up_y_arm_1",
@@ -107,9 +148,7 @@ def get_data_from_image_files(list_of_files):
                 dict(
                     zip(
                         filename_keys,
-                        list(
-                            re.match(filename_pattern, os.path.basename(file)).groups()
-                        ),
+                        list(re.match(filename_pattern, os.path.basename(file)).groups()),
                     )
                 )
             ),
@@ -122,12 +161,7 @@ def get_data_from_image_files(list_of_files):
 
 def select_images(image_data, key_dict):
     # Currently we support only two keys
-    assert all(
-        [
-            key == "src_subject_id" or key == "eventname"
-            for key, value in key_dict.items()
-        ]
-    )
+    assert all([key == "src_subject_id" or key == "eventname" for key, value in key_dict.items()])
     response = [
         image_row
         for image_row in image_data
@@ -180,19 +214,20 @@ def select_items(list_of_row_dicts, key_dict):
     return response
 
 # +
-# Functions for computing summary statistics for csv data
+# Functions for computing summary statistics for KSADS csv data
 
 
 def row_dicts_to_counts_for_each_column(row_dicts):
     column_dict = {key: dict() for row in row_dicts for key in row.keys()}
     for row in row_dicts:
         for column_header, value in row.items():
-            # column_dict[column_header] = column_dict.get(column_header, dict())
-            if value == "555" or value == "888":
-                value = ""
-            column_dict[column_header][value] = (
-                column_dict[column_header].get(value, 0) + 1
-            )
+            # 555 = "Not administered in the assessment",
+            # so assume the same as "".  (TODO: Right?)
+            value = "" if value == "555" else value
+            # 888 = "Question not asked due to primary question response (branching logic)",
+            # so assume it was obviously a "0".  (TODO: Right?)
+            value = "0" if value == "888" else value
+            column_dict[column_header][value] = column_dict[column_header].get(value, 0) + 1
     # Sort each value dict by its keys for easier reading
     column_dict = {
         key: dict(sorted(value.items(), key=lambda item: item[0], reverse=False))
@@ -224,7 +259,6 @@ def entropy_of_all_columns(all_columns):
 
 def find_interesting_entropies(file_mh_y_ksads_ss):
     # Find some KSADS data columns with high entropy.
-    # TODO: Should we be treating the value '888' or '' specially?
     row_dicts_mh_y_ksads_ss = csv_file_to_row_dicts(file_mh_y_ksads_ss)
     print("Read done")
 
@@ -234,9 +268,7 @@ def find_interesting_entropies(file_mh_y_ksads_ss):
     print("Column counting done")
 
     entropies = entropy_of_all_columns(counts_for_each_column_mh_y_ksads_ss)
-    sorted_entropies = dict(
-        sorted(entropies.items(), key=lambda item: item[1], reverse=True)
-    )
+    sorted_entropies = dict(sorted(entropies.items(), key=lambda item: item[1], reverse=True))
     sorted_entropies = {
         key: (value, counts_for_each_column_mh_y_ksads_ss[key])
         for key, value in sorted_entropies.items()
@@ -249,20 +281,28 @@ def find_interesting_entropies(file_mh_y_ksads_ss):
 # +
 # Find interesting KSADS data
 
+file_mh_y_ksads_ss = "mental-health/mh_y_ksads_ss.csv"
 if True:
-    # We want to re-compute interesting_ksads
+    # User requests that we re-compute `interesting_ksads`
+    if False:
+        # User requests that we re-compute `sorted_entropies`
+        try:
+            del sorted_entropies
+        except NameError:
+            pass
     try:
         sorted_entropies
         print("Using cached value for sorted_entropies")
     except NameError:
-        # find_interesting_entropies is slow, so only compute sorted_entropies if we haven't already
-        sorted_entropies = find_interesting_entropies(file_mh_y_ksads_ss)
+        full_path = os.path.join(core_directory, file_mh_y_ksads_ss)
+        # find_interesting_entropies is slow
+        sorted_entropies = find_interesting_entropies(full_path)
     number_wanted = 20
-    # print(f"Distribution details: {list(sorted_entropies.items())[:number_wanted]}")
+    print(f"Distribution details: {list(sorted_entropies.items())[:number_wanted]}")
     interesting_ksads = list(sorted_entropies.keys())[:number_wanted]
     print(f"{interesting_ksads = }")
 else:
-    # Previous expression returns
+    # With "555" and "888" both going to "", the interesting_ksads computation gives:
     interesting_ksads = [
         "ksads_22_142_t",  # Symptom - Insomnia, Past
         "ksads_22_970_t",  # Diagnosis - SLEEP PROBLEMS, Past
@@ -285,12 +325,49 @@ else:
         "ksads_1_3_t",  # Symptom - Irritability, Present
         "ksads_1_842_t",  # Diagnosis - Major Depressive Disorder, Past (F32.9)
     ]
+    # With "555" going to "" and "888" going to "0", the interesting_ksads computation gives:
+    interesting_ksads = [
+        "ksads_1_187_t",  # Symptom - No two month symptom-free interval, Present
+        "ksads_1_188_t",  # Symptom - No two month symptom-free interval, Past
+        "ksads_22_142_t",  # Symptom - Insomnia, Past
+        "ksads_22_970_t",  # Diagnosis - SLEEP PROBLEMS, Past
+        "ksads_2_11_t",  # Symptom - Explosive Irritability, Past
+        "ksads_2_222_t",  # Symptom - Lasting at least 4 days, Past
+        "ksads_1_184_t",  # Symptom - Impairment in functioning due to depression, Past
+        "ksads_1_2_t",  # Symptom - Depressed Mood, Past
+        "ksads_2_13_t",  # Symptom - Decreased Need for Sleep, Past
+        "ksads_1_6_t",  # Symptom - Anhedonia, Past
+        "ksads_1_160_t",  # Symptom - Fatigue, Past
+        "ksads_1_162_t",  # Symptom - Concentration Disturbance, Past
+        "ksads_2_220_t",  # Symptom - Lasting at least one week, Past
+        "ksads_1_156_t",  # Symptom - Insomnia when depressed, Past
+        "ksads_1_174_t",  # Symptom - Psychomotor Agitation in Depressive Disorder, Past
+        "ksads_2_216_t",  # Symptom - Impairment in functioning due to bipolar, Past
+        "ksads_2_208_t",  # Symptom - Psychomotor Agitation in Bipolar Disorder, Past
+        "ksads_2_206_t",  # Symptom - Increased Energy, Past
+        "ksads_22_141_t",  # Symptom - Insomnia, Present
+        "ksads_22_969_t",  # Diagnosis - SLEEP PROBLEMS, Present
+    ]
+
+ksads_vars = [[file_mh_y_ksads_ss, interesting_ksads]]
+
+# +
+# Find all images for which we have tabular data
+
+set_of_pairs_for_each_key = (
+    {(row["src_subject_id"], row["eventname"]) for row in row_dict if row[key] != ""}
+    for tablename, list_of_keys in independent_vars
+    for row_dict in (csv_file_to_row_dicts(os.path.join(core_directory, tablename)),)
+    for key in list_of_keys
+)
+set_intersection = functools.reduce(set.intersection, set_of_pairs_for_each_key)
+print(f"{len(set_intersection) = }")
+print(f"{set(list(set_intersection)[:20]) = }")
+# TODO: If necessary, try fewer independent_vars so that we get more images
 # -
 
 # A random subset of all image files
-list_of_image_files = random.sample(
-    get_list_of_image_files(coregistered_images_directory), 10
-)
+list_of_image_files = random.sample(get_list_of_image_files(coregistered_images_directory), 10)
 image_data = get_data_from_image_files(list_of_image_files)
 print(f"file name parsing = {[image[1] for image in image_data]!r}")
 
